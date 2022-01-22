f1afa33650adf2560dfab77177975f0f91c1397c
Nilton Carlos <niltonfirminofs@gmail.com>
2022-01-22 17:00:41 -0300

    Playground Class

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,3 +1,13 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  //instaciando Classes
+  var person = Person( name: 'Nilton', age: 34);
+
+  print(person.name);
+}
+
+class Person {
+  final String name;
+  final int age;
+
+  Person({required this.name, required this.age});
 }
